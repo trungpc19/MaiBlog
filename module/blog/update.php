4d471444dd21c1265ec9137ba63f355c337f65a6
Chigger <phamtrung962003@gmail.com>
2024-11-07 00:16:25 +0700

    Fix SQL Injection in Function update Blog

--- module/blog/update.php
+++ module/blog/update.php
@@ -1,6 +1,6 @@
  <?php
 
-    $blog_id = $_GET['blog_id'];
+    $blog_id = intval($_GET['blog_id']);
     $query = "SELECT * FROM blogs WHERE blog_id = $blog_id";
     $result = $conn->query($query);
     $blog = $result->fetch_assoc();
